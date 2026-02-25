dd81308399af90f3fe9b20cba2c06edc9153e7c1
Mufanc <mufanc.xyz@gmail.com>
2026-02-26 01:54:29 +0800

    refactor: move LookupConfig from query.rs to resolver.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-mod query;
 mod resolver;
 mod result;
 
-pub use query::*;
 pub use resolver::*;
 pub use result::*;
--- src/resolver.rs
+++ src/resolver.rs
@@ -1,4 +1,3 @@
-use crate::query::LookupConfig;
 use crate::result::{ResolverError, ResolverResult};
 use object::{Object, ObjectSection, ObjectSymbol, SectionIndex};
 use std::fs;
@@ -20,6 +19,33 @@ pub struct Section {
     pub name: Box<str>,
     pub addr: usize,
     pub file_range: Option<(usize, usize)>,
+}
+
+#[derive(Copy, Clone)]
+pub struct LookupConfig<'a> {
+    pub query: &'a str,
+    pub prefix: bool,
+    pub debugdata: bool,
+}
+
+impl<'a> LookupConfig<'a> {
+    pub fn new(query: &'a str) -> Self {
+        Self {
+            query,
+            prefix: false,
+            debugdata: false,
+        }
+    }
+
+    pub fn with_prefix(mut self, prefix: bool) -> Self {
+        self.prefix = prefix;
+        self
+    }
+
+    pub fn with_debugdata(mut self, debugdata: bool) -> Self {
+        self.debugdata = debugdata;
+        self
+    }
 }
 
 pub struct SymbolResolver<'a> {
